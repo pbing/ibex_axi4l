ac15c54d33975a4bdb261f78b795f0c440a4a7fa
Bernd Beuster <bernd.beuster@gmail.com>
2025-05-02 18:27:13 +0200

    Improve debugging

--- soc/fpga/arty-a7-100/sw/led/led.c
+++ soc/fpga/arty-a7-100/sw/led/led.c
@@ -6,14 +6,14 @@ const unsigned long long CLK_FIXED_FREQ_HZ = 50000000ULL;
 const unsigned int TICK_TIME = 10;    // 10 ms
 const unsigned int BLINK_TIME = 1000; // 1000 ms
 
-static volatile uint32_t *SW        = (uint32_t*)0x10000000;
-static volatile uint32_t *LEDRGB    = (uint32_t*)0x10001000;
-static volatile uint32_t *LED       = (uint32_t*)0x10002000;
-static volatile uint32_t *BTN       = (uint32_t*)0x10003000;
-static volatile uint32_t *MTIME     = (uint32_t*)0x10004000;
-static volatile uint32_t *MTIMEH    = (uint32_t*)0x10004004;
-static volatile uint32_t *MTIMECMP  = (uint32_t*)0x10004008;
-static volatile uint32_t *MTIMECMPH = (uint32_t*)0x1000400c;
+static volatile uint32_t* SW        = (uint32_t*)0x10000000;
+static volatile uint32_t* LEDRGB    = (uint32_t*)0x10001000;
+static volatile uint32_t* LED       = (uint32_t*)0x10002000;
+static volatile uint32_t* BTN       = (uint32_t*)0x10003000;
+static volatile uint32_t* MTIME     = (uint32_t*)0x10004000;
+static volatile uint32_t* MTIMEH    = (uint32_t*)0x10004004;
+static volatile uint32_t* MTIMECMP  = (uint32_t*)0x10004008;
+static volatile uint32_t* MTIMECMPH = (uint32_t*)0x1000400c;
 
 __attribute__((interrupt)) void timer_interrupt_handler(void) {
   // sense buttons and switch RGB LEDs with color encoded in SW[2:0]
@@ -44,8 +44,12 @@ __attribute__((interrupt)) void timer_interrupt_handler(void) {
 };
 
 int main(int argc, char **argv) {
-  //asm("csrci 0x7c0, 1"); // disable icache
+
+#if DEBUG
+  asm("csrci 0x7c0, 0"); // disable icache
+#else
   asm("csrsi 0x7c0, 1"); // enable icache
+#endif
 
   *LEDRGB = 0;
   *LED = 0;
